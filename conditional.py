716db80484f65007d53a618b5210fb1da2441a64
kimjinseop13579 <kimjinseop090@gmail.com>
2026-04-22 16:01:15 +0900

    20260422

--- conditional.py
+++ conditional.py
@@ -39,6 +39,8 @@
 # like 좋아요 변수의 값이 100 이상이면 spot 등록 출력
 # 좋아요 변수의 값이 10 이하라면 관심 spot 출력
 
+##   정수로 변환 int(값)  실수로 변환 float(값)  문자열로 변환 str(값)
+
 
 # like = int(input("좋아요 수 : "))
 
@@ -74,27 +76,108 @@
 #     print("없다")
 
 #startswith() 함수 시작 문자열 비교
-word = "이창호 님께서 입장 하셨습니다."
-if word.startswith('이창호'):
-    print("신원 확인")
-else:
-    print("이창호 님이 아닙니다")
+# word = "이창호 님께서 입장 하셨습니다."
+# if word.startswith('이창호'):
+#     print("신원 확인")
+# else:
+#     print("이창호 님이 아닙니다")
 
-# 대소문자 변환
-word = "i like banana"
-print(word.upper()) #대문자
-print(word.lower()) #소문자
-print(word.title()) #대문자 - 단어의 첫글자만
+# # 대소문자 변환
+# word = "i like banana"
+# print(word.upper()) #대문자
+# print(word.lower()) #소문자
+# print(word.title()) #대문자 - 단어의 첫글자만
 
-#공백제거 - 개발시 필요필수 (이거때문에 오류나면 골치아픔)
-word = " banana "
-print(word) #공백제거 없이
-print(word.strip()) #양쪽공백 제거
-print(word.lstrip()) #왼쪽 공백제거
-print(word.rstrip()) #오른쪽 공백 제거
+# #공백제거 - 개발시 필요필수 (이거때문에 오류나면 골치아픔)
+# word = " banana "
+# print(word) #공백제거 없이
+# print(word.strip()) #양쪽공백 제거
+# print(word.lstrip()) #왼쪽 공백제거
+# print(word.rstrip()) #오른쪽 공백 제거
 
-#찾기
-word = "찬용이는 진섭이보다 지금이 좋다고 한다."
-print(word.find("진섭")) #있다면 위치 반환(인덱스) 없으면 -1
+# #찾기
+# word = "찬용이는 진섭이보다 지금이 좋다고 한다."
+# print(word.find("진섭")) #있다면 위치 반환(인덱스) 없으면 -1
 
-print(word.index("동렬")) #인덱스 반환, 없으면 에러
+# print(word.index("동렬")) #인덱스 반환, 없으면 에러
+
+# #문자열 바꾸기 .replace("현재문자열에서 변경 할 문자열", "교체할 문자열")
+# word = word.replace("찬용이","성현이")
+# print(word)
+
+# #문자열 나누기 - 배열
+# text = "도헌-지연-동렬-진섭"
+# result = text.split("-")
+# print(result)
+
+# # 배열을 하나의 문자열로 합치기
+# text = ",".join(result) # 배열값 하나하나에 , 를 넣어서 하나의 문자열 만들기
+# print(text)
+
+# #숫자냐 아니냐
+# text = "123456"
+# print(text.isdigit()) #문자열을 숫자로 변환하기전에 확인하는용도 isdigit (is 디지트)
+
+# #문자 종류 확인
+# text1 = "tomato"
+# text2 = "  "
+# text3 = "사월22"
+# print(text2.isalpha()) #문자만 있냐 isalpha (is 알파)
+# print(text1.isspace()) #공백이 있냐 isspace (is 스페이스)
+# print(text3.isalnum()) #문자+숫자냐 isalnum (is 알 넘)
+
+# #문자열 정렬
+# text = "15"
+# print(text.zfill(6)) # 함수()안에 자릿수 넣기 zfill (z 필)
+# print(text.rjust(4)) # 오른쪽 R 왼쪽 L 정렬 just
+# print(text.ljust(5))
+
+# 문제1. - 공백제거와 소문자 변환 하려면??
+# input 으로 입력 받아서 공백제거와 소문자 변환을 하세요
+
+# text = input("대문자 입력 : ")
+# print(text.strip().lower())
+
+# 문제2. "행복,우울,기쁨,슬픔,화남"
+# 위 문자열을 나누어 보시오
+
+# testtext = "행복,우울,기쁨,슬픔,화남"
+# result = testtext.split(",")
+# print(result)
+
+# 문제3. 회원가입시 이메일 입력을하는데 특정 주소만 가능하다.
+# naver.com, gmail.com, nate.com, daum.net
+# 위 4개 만 가능하다 input으로 이메일을 입력받아서 가입 가능한지 불가능 한지 출력
+
+# email = input("이메일 입력 : ").strip().lower()
+# if email.endswith("naver.com"):
+#     print("가입 가능")
+# elif email.find("gmail.com") != -1 :
+#     print("가입 가능")
+# elif email.split("@")[1] == "nate.com":
+#     print("가입 가능")
+# elif email.endswith("daum.net") : 
+#     print("가입가능")
+# else:
+#     print("가입 불가능")
+
+# 문제 4. 금액 계산하기
+# 각 업체별로 입금이 되었다. 총액이 얼마인지 출력하세요
+쿠팡 = "135,900원"
+네이버 = "540,000원"
+오드론 = "2,340,090원"
+
+쿠팡 = 쿠팡.replace("원","").replace(",","")
+네이버 = 네이버.replace("원","").replace(",","")
+오드론 = 오드론.replace("원","").replace(",","")
+
+쿠팡 = int(쿠팡)
+네이버 = int(네이버)
+오드론 = int(오드론)
+
+add = str(쿠팡+네이버+오드론)
+print("총금액 : "+add+"원")
+
+
+
+
---
+++ loop.py
@@ -0,0 +1,165 @@
+
+
+# print("숫자 : 1")
+# print("숫자 : 2")
+# print("숫자 : 3")
+# print("숫자 : 4")
+# print("숫자 : 5")
+
+# # 5번 반복하는 반복문
+# for i in range(101,1,-1):
+#     print("숫자 : "+str(i)) 
+
+# print("========================================================================")
+
+# for ch in "hello":
+#     print(ch)
+
+# for name in ["차도헌", "박지연", "이성찬", "김진숙", "이동렬", "김현규"] :
+#     if name.startswith("이"):
+#         print(name)
+
+# # 문제1. 1부터 10까지의 총합을 구하시오 반복문을 사용해서
+
+# num = 0
+
+# for add in range(1,11):
+#     num += add
+
+# print("총합 : "+str(num))
+
+# # 문제 2. ["15,000", "13,000", "8,700", "9,000", "25,000"]
+# # 배열에 출금 금액들이 저장되어있다.
+# # 만원 이상 금액들에 대해 출력하세요
+
+# print("========================================================================")
+
+# money = ["15,000", "13,000", "8,700", "9,000", "25,000"]
+# for i in range(5):
+#     mon = int(money[i].replace(",",""))
+#     if mon > 10000:
+#         print(str(mon)+"원")
+
+# print("========================================================================")
+
+# for i in range(len(money)):
+#     print("금액 : ",money[i])
+
+# for i, v in enumerate(money):
+#     print(i+1,v)
+
+# print("========================================================================")
+
+# #문제 3. [89,56,78,92,61,96,83,74]
+# #203호 학생들의 성적이다. 성적의 총합과 평균을 출력하세요
+# # 80점 이상인 학생들의 위치(인덱스)도 출력
+
+# score = [89,56,78,92,61,96,83,74]
+# total = 0
+# ave = 0
+
+# for s in range(len(score)):
+#     total += score[s]
+#     if score[s] >= 80 :
+#         print("80점 이상 학생 위치 : ",s," 점수 : ",score[s])
+
+# ave = total/len(score)
+
+# print ("점수 총합 : ",total)
+# print ("점수 평균 : ",ave)
+
+# while 문은 조건식이 참인경우 동작하기 때문에
+# 쉽게 무한루프에 들어갈수 있다
+# 하여 while 문 사용시 중단시킬수 있는 break 를 같이 사용하는게 좋다.
+
+# num = 5
+# while num > 2:
+#     print("2보다 크다")
+#     break
+
+# while True:
+#     num += 1
+#     if num == 7: continue # 건너뛰기
+#     print(num)
+#     if num==10: break
+
+# print("=============================================")
+# while True:
+#     cmd = input("명령어 입력 : ").strip().lower()
+#     if cmd == "history":
+#         print("모든 기록 출력")
+#     elif cmd == "mkdir":
+#         print("새로운 폴더 만들기")
+#     elif cmd =="remove":
+#         print("파일 삭제")
+#     elif cmd == "exit":
+#         print("종료")
+#         break
+#     else:
+#         print("존재하지 않는 명령어 입니다.")
+
+#파이썬 랜덤 사용
+import random
+
+num = random.randint(1,10) # random.randint(random + int)
+
+# 동전 앞면 뒷면 맞추기 게임 만들기
+# while True:
+#     user = input("입력 : ").strip()
+#     coin = random.randint(1,2)
+#     print(coin)
+#     if int(user) == coin :
+#         print("성공")
+#     else:
+#         print("실패")
+    
+#     if user == "exit":
+#         break
+
+n = random.randrange(1,10) #1~9
+game = ["가위","바위","보"]
+w = 0
+l = 0
+d = 0
+print("가위 바위 보!!! 경기~~~~!!!!!!!!! 시작~~~~~~~~~~~~~~~~~~~~~~!!!! 하겠습니다!!!!!!!!!!!!!!!!!!!!!!")
+while True:
+    print("점수판")
+    print(" 승 : ",w," 패 : ",l,"무 : ",d)
+    
+    user = input("가위 바위 보!! : ").strip()
+
+    n = random.choice(game)
+    print(n)
+    print(user)
+    if (user =="가위" and n == "보") or (user == "바위" and n == "가위") or (user == "보" and n == "바위"):
+        print("승리 !!!!!!")
+        w += 1
+    elif user == n :
+        print("무승부!!!!!!!")
+        d += 1
+    elif user == "그만" :
+        print("그만!!!! 경기!!!!!!!!!!!!!!!!! 끝났습니다~~~!!!!!.")
+        break
+    else :
+        print("패배!!!!!!!")
+        l += 1
+
+# cidx = game.index(com)
+# uidx = game.indx(user)
+
+# for i in range(5):
+#     com = random.choice(game)
+#     user = input("가위 바위 보 : ").strip()
+#     print("컴퓨터 : ",com,"나",user)
+
+#     comp = cidx - uidx #유저와 컴의 가위바위보 값 비교
+# #가위 -0 바위-1 보-2 유저가 
+
+#     if com == user:
+#         print("비김")
+#     elif comp == -1 or comp==2:
+#         print("승리")
+#     else:
+#         print("패배")
+
+
